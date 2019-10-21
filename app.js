f80acba76eae29d3f6f2c99a1ac2715c52ee54a3
JeffDorling <jdorling15@gmail.com>
2019-10-21 15:34:07 +1000

    server setup and tested on my machine

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-​
 const port = 3000;
+const mongoose = require('mongoose');
+
+const app = express();
 
 const dbConn = 'mongodb://localhost/chores';
 // Set four properties to avoid deprecation warnings:
@@ -21,14 +22,11 @@ mongoose.connect(dbConn, {
         }
     });
 
-const app = express();
-​
+
 //Middleware
 app.use(cors())
 app.use(bodyParser.json())
 
-//
-​
 app.listen(port, () => {
-    console.log(`Blog express app listening on port ${port}`);
+    console.log(`Chores app listening on port ${port}`);
 })
