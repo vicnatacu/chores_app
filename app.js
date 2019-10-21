84a051f998d0660aed40ab4e0c128a4a925adb21
absmohamed <cabdullahidaud@Hotmail.com>
2019-10-21 15:37:23 +1000

    comitting change

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-​
+//const cors = require('cors');
+//const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
 const port = 3000;
 
-const dbConn = 'mongodb://localhost/chores';
-// Set four properties to avoid deprecation warnings:
-mongoose.connect(dbConn, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    },
-    (err) => {
-        if (err) {
-            console.log('Error connecting to database', err);
-        } else {
-            console.log('Connected to database!');
-        }
-    });
+// const dbConn = 'mongodb://localhost/chores';
+// // Set four properties to avoid deprecation warnings:
+// mongoose.connect(dbConn, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useFindAndModify: false,
+//         useCreateIndex: true
+//     },
+//     (err) => {
+//         if (err) {
+//             console.log('Error connecting to database', err);
+//         } else {
+//             console.log('Connected to database!');
+//         }
+//     });
 
 const app = express();
 ​
 //Middleware
-app.use(cors())
-app.use(bodyParser.json())
+//app.use(cors())
+//app.use(bodyParser.json())
 
 //
 ​
