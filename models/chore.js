95a30188d4ecd45cd316fb862ef26eefcae8a40c
vicnatacu <vicki@mvbmb.com>
2019-10-21 15:15:43 +1000

    table schema

---
+++ models/chore.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Chore = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    due_date: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model("Chore", Chore);
