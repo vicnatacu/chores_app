dc775029d1a7c252236d9ce83f285e7c2cf1bba0
vicnatacu <vicki@mvbmb.com>
2019-10-22 10:35:35 +1000

    early user authentication

--- routes/chores_routes.js
+++ routes/chores_routes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const {
     makeChore,
+    removeChore,
+    updateChore,
+    verifyOwner,
     userAuthenticated
 } = require('../controllers/chores_controller');
 
+
+// For post, delete, put -require authenticated user
+router.use(userAuthenticated);
 
 // CREATE
 // POST on '/posts'
@@ -15,11 +21,11 @@ router.post("/", userAuthenticated, makeChore);
 // DELETE
 // DELETE on '/posts/:id'
 // Deletes a post with id
-// router.delete("/:id", userAuthenticated, removeChore);
+// router.delete("/:id", verifyOwner, removeChore);
 
 // UPDATE
 // PUT on 'posts/:id'
 // Updates a post with id
-// router.put("/:id", userAuthenticated, changeChore);
+// router.put("/:id", verifyOwner, changeChore);
 
 module.exports = router;
