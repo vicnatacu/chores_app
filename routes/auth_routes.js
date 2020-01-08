39b3a1681d123b3cb699f72877b357ef2f851f14
vicnatacu <vicki@mvbmb.com>
2020-01-08 19:10:29 +1000

    untracked changes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {register, login, logout} = require('../controllers/auth_controller');
-
+const {removeUser, changeUser} = require('../controllers/user_controller')
 //User registeration
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
+
+
+router.put('user/:id', changeUser);
+router.delete('user/:id', removeUser)
 
 
 module.exports = router;
